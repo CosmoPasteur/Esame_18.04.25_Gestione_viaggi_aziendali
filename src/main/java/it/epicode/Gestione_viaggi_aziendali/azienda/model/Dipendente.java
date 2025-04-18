cf9ee2dd806c9b16671a80b204f01329875686c5
“Cosimo <pastore_cosimo@yahoo.it>
2025-04-18 11:33:55 +0200

    @GeneratedValue(strategy = GenerationType.SEQUENCE)

---
+++ src/main/java/it/epicode/Gestione_viaggi_aziendali/azienda/model/Dipendente.java
@@ -0,0 +1,4 @@
+package it.epicode.Gestione_viaggi_aziendali.azienda.model;
+
+public class Dipendente {
+}
---
+++ src/main/java/it/epicode/Gestione_viaggi_aziendali/azienda/model/Viaggio.java
@@ -0,0 +1,15 @@
+package it.epicode.Gestione_viaggi_aziendali.azienda.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Viaggio {
+    @Id
+    @GeneratedValue(strategy =)
+}
